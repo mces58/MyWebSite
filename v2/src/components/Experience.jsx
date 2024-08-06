e40fc78505421b278fcddc58868adbcc4ca9a367
mces58 <ceser446@gmail.com>
2024-08-06 17:45:57 +0300

    feat: add Experience component and ExperienceCard subcomponent

--- v2/src/components/Experience.jsx
+++ v2/src/components/Experience.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { VerticalTimeline } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+import { motion } from 'framer-motion';
+
+import ExperienceCard from '@/components/ExperienceCard';
+import { experiences } from '@/constants';
+import { styles } from '@/styles';
+import { textVariant } from '@/utils/motion';
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>What I have done so far</p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Work Experience.</h2>
+      </motion.div>
+
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={`experience-${index}`} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
 };
 
 export default Experience;
