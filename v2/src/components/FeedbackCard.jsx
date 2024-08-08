25cc10d0b1eae93a8ee73b03e282e781db2384f6
mces58 <ceser446@gmail.com>
2024-08-08 19:58:48 +0300

    feat: optimize Feedbacks component with motion and add FeedbackCard subcomponent

---
+++ v2/src/components/FeedbackCard.jsx
@@ -0,0 +1,47 @@
+import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
+
+import { fadeIn } from '@/utils/motion';
+
+const FeedbackCard = ({ index, testimonial, name, designation, company, image }) => {
+  return (
+    <motion.div
+      variants={fadeIn('', 'spring', index * 0.5, 0.75)}
+      className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
+    >
+      <p className="text-white font-black text-[48px]">&quot;</p>
+
+      <div className="mt-1">
+        <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
+
+        <div className="mt-7 flex justify-between items-center gap-1">
+          <div className="flex-1 flex flex-col">
+            <p className="text-white font-medium text-[16px]">
+              <span className="blue-text-gradient">@</span> {name}
+            </p>
+            <p className="mt-1 text-secondary text-[12px]">
+              {designation} of {company}
+            </p>
+          </div>
+
+          <img
+            src={image}
+            alt={`feedback_by-${name}`}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default FeedbackCard;
+
+FeedbackCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  testimonial: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  designation: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
--- v2/src/components/Feedbacks.jsx
+++ v2/src/components/Feedbacks.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { motion } from 'framer-motion';
+
+import FeedbackCard from '@/components/FeedbackCard';
+import { testimonials } from '@/constants';
+import { styles } from '@/styles';
+import { textVariant } from '@/utils/motion';
 
 const Feedbacks = () => {
-  return <div>Feedbacks</div>;
+  return (
+    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
+      <div className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Feedbacks;
