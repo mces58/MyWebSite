dea420e51996799ce50683b4d4fa19cb0ff5ff8d
mces58 <ceser446@gmail.com>
2024-08-09 12:04:49 +0300

    chore: Update .gitignore and Contact component

--- v2/src/components/Contact.jsx
+++ v2/src/components/Contact.jsx
@@ -38,9 +38,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'JavaScript Mastery',
+          to_name: 'mces',
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
+          to_email: 'dinosaur.temp@gmail.com',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
